512c7ee0909ba2abaf78fd1b7e21fa3e429eebb9
Magnanimoose <57162016+Magnanimoose@users.noreply.github.com>
2020-07-05 19:42:26 -0400

    Delete Project 1 - useful scratch r2.py

--- Project 1 - useful scratch r2.py
+++
@@ -1,9 +0,0 @@
-from math import log10
-from scipy import stats
-from scipy.stats import kstest
-from scipy.stats import f
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy import integrate
-
-git help()
